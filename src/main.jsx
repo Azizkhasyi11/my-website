36dbbaf743c6ce0d60ae8b79a7d1f73ade1f1f89
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-06-08 12:48:44 +0700

    chore: Remove unused "source" property from reactSnap configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,26 +5,26 @@ import { RouterProvider } from 'react-router-dom'
 import Routes from './router/Routes.jsx';
 import '@fontsource/fira-code';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <RouterProvider router={Routes} />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={Routes} />
+  </React.StrictMode>,
+)
 
-if (rootElement?.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <RouterProvider router={Routes} />
-    </React.StrictMode>
-  );
-} else {
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={Routes} />
-    </React.StrictMode>
-  )
-}
+// if (rootElement?.hasChildNodes()) {
+//   hydrateRoot(
+//     rootElement,
+//     <React.StrictMode>
+//       <RouterProvider router={Routes} />
+//     </React.StrictMode>
+//   );
+// } else {
+//   root.render(
+//     <React.StrictMode>
+//       <RouterProvider router={Routes} />
+//     </React.StrictMode>
+//   )
+// }
